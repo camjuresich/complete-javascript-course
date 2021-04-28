172c1e2708787f5d86151ed93d5328e3c5e95f0c
Cameron <camjuresich@gmail.com>
2021-04-28 13:06:13 -1000

    roll--dice btn rolls a random number, does not store

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let turn = 0;
+let diceCurr;
+let randy = () => Math.floor((Math.random() * 6) + 1);
+//const player1 = document.getElementById()
+const rollBtn = document.getElementsByClassName('btn--roll')[0];
+const holdBtn = document.getElementsByClassName('btn--hold')[0];
+
+rollBtn.addEventListener("click", function(){
+
+  diceCurr = randy();
+  const dice = document.getElementsByClassName('dice')[0]
+  dice.src = `dice-${diceCurr}.png`
+  console.log(diceCurr)
+})
